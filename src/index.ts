7403fec4bc091a537b63f1a3c93ea0522a5aa6cc
victorklippel <victor.klippel@quaestum.com.br>
2022-03-23 20:52:42 +0000

    changes librarie

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { default as Card } from './Card';
+export { default as TextComponent} from './TextComponent';
+export { default as TextInputComponent} from './Input';
+export { default as Modal} from './Modal';
